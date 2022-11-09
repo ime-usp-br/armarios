dd45e51cc21780a3b21aac2fc6fcd9d42362ab6e
Mickael <mickaelrodrigues@usp.br>
2022-11-09 17:56:59 -0300

    Adicionado rotas de login local

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArmarioController;
+use App\Http\Controllers\LoginController;
 
 
 
 Route::resource('armarios',ArmarioController::class);
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
