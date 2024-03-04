38b0bffb0086078a2b203420500764177973e177
Mickael Rodrigues <mickaelrodrigues@usp.br>
2024-03-04 12:00:03 -0300

    Adicionado o command que atualiza a tabela da data de defesa dos alunos

--- app/Console/Commands/AtualizarDataDefesa.php
+++ app/Console/Commands/AtualizarDataDefesa.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use DateTime;
+use Log;
+use App\Models\Emprestimo;
+use Uspdev\Replicado\DB;
+use Carbon\Carbon;
 
 class AtualizarDataDefesa extends Command
 {
@@ -11,14 +16,14 @@ class AtualizarDataDefesa extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'command:atualizardatadefesa';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Atualiza a data de defesa de um aluno de pós, verificando se ele tem permissão para realizar empréstimo.';
 
     /**
      * Create a new command instance.
@@ -37,6 +42,52 @@ class AtualizarDataDefesa extends Command
      */
     public function handle()
     {
-        return 0;
+        Log::info('Comando Sincronizador em execução...');
+
+        $dt = new DateTime();
+        $atualizacoes = 0;
+        $relatorio = [];
+
+        $emprestimos = Emprestimo::with('user')->get();
+        $codpesList = $emprestimos->pluck('user.codpes')->unique()->toArray();
+
+        $query = "SELECT AGP.codpes, MAX(AGP.dtadfapgm) AS ultima_data"; 
+        $query .= " FROM AGPROGRAMA AS AGP";
+        $query .= " JOIN ( VALUES ";
+        foreach(range(0, count($codpesList)-1) as $i){
+            $query .= "('".$codpesList[$i]."')";
+            if($i != count($codpesList)-1){
+                $query .= ",";
+            }
+        }
+        $query .= " ) AS TEMP(codpes)";
+        $query .= " ON AGP.codpes = TEMP.codpes ";
+        $query .= " GROUP BY AGP.codpes ";
+        $data = DB::fetchAll($query);
+        
+        foreach ($emprestimos as $emprestimo) {
+            $codpes = $emprestimo->user->codpes;
+
+            $dataAluno = collect($data)->firstWhere('codpes', $codpes);
+
+            if ($dataAluno and $dataAluno['ultima_data'] != null) {
+                $emprestimo->dataprev = Carbon::createFromFormat('Y-m-d h:i:s', $dataAluno['ultima_data'])->format('d/m/Y');
+                $emprestimo->save();
+
+                $atualizacoes++;
+
+                $relatorio[$atualizacoes]['id_emprestimo'] = $emprestimo->id;
+                $relatorio[$atualizacoes]['codpes_aluno'] = $codpes;
+                $relatorio[$atualizacoes]['dataprev_atualizada'] = $emprestimo->dataprev;
+            }
+        }
+
+        $interval = $dt->diff(new DateTime());
+        Log::info($interval->format('Duração: %Hh %Im %Ss'));
+        Log::info("Total de atualizações: " . $atualizacoes);
+        Log::info('########## Comando Sincronizador finalizado!');
+
+        
     }
+
 }
